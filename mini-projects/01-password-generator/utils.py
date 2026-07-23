bac39e05c5e10c11e8dfa62a38d6bf39b3626378
Aditysinh Rathod <aditysinhrathod0610@gmail.com>
2026-07-23 15:26:58 +0530

    Create utils.py

---
+++ mini-projects/01-password-generator/utils.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+def generate_password(length):
+
+    characters = (
+        string.ascii_letters
+        + string.digits
+        + string.punctuation
+    )
+
+    return "".join(random.choice(characters) for _ in range(length))
